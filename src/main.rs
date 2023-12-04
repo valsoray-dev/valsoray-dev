6022d651b469adbbdbab31e08f9e31021bb6ad4c
valsoray-dev <77742707+valsoray-dev@users.noreply.github.com>
2023-12-04 02:51:59 +0200

    changed project structure + `dotenvy`

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,48 @@
-use actix_files::Files;
+use actix_files::{Files, NamedFile};
 use actix_web::{
-    get, http::header::ContentType, middleware, App, HttpRequest, HttpResponse, HttpServer,
-    Responder,
+    get,
+    http::{header::ContentType, StatusCode},
+    middleware, App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
-use dotenv::dotenv;
 use log::info;
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(include_str!("./static/index.html"))
+    let path = "./assets/index.html";
+    NamedFile::open_async(path).await
 }
 
 #[get("/admin")]
 async fn admin(req: HttpRequest) -> impl Responder {
     if !req.headers().contains_key("hx-request") {
-        let image_data = include_bytes!("./static/404.jpg");
-        return HttpResponse::NotFound()
-            .content_type(ContentType::jpeg())
-            .body(&image_data[..]);
+        let path = "./assets/404.jpg";
+        return NamedFile::open_async(path)
+            .await
+            .unwrap()
+            .into_response(&req)
+            .customize()
+            .with_status(StatusCode::NOT_FOUND);
     }
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body("<video src='/rickroll.mp4' autoplay loop style='width: 100%; height: 100%'></video>")
+        .customize()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-    pretty_env_logger::init();
+    pretty_env_logger::formatted_builder()
+        .parse_filters(&dotenvy::var("RUST_LOG").unwrap_or("info".to_string()))
+        .init();
 
-    let port = std::env::var("PORT").expect("PORT must be set");
-    info!("Server is running at http://localhost:{}", &port);
+    let port = dotenvy::var("PORT").expect("PORT must be set");
+    info!("Server is running at http://localhost:{}", port);
 
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(admin)
-            .service(Files::new("/", "./src/static").prefer_utf8(true))
+            .service(Files::new("/", "./assets").prefer_utf8(true))
             .wrap(middleware::Logger::new("%a | %r [%s] | Time: [%T]"))
     })
     .bind(("0.0.0.0", port.parse::<u16>().unwrap()))?
