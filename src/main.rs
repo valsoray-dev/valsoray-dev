d1248c031e0fd74f5da6c2536b7c2ccbd26a535c
Yaroslav Pietukhov <77742707+valsoray-dev@users.noreply.github.com>
2024-11-26 13:34:11 +0200

    move rickroll html to `assets/rickroll.html`

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,11 @@ async fn rickroll(req: HttpRequest) -> impl Responder {
         return HttpResponse::NotFound()
             .content_type(ContentType::jpeg())
             .insert_header((CONTENT_LOCATION, "https://http.cat/404.jpg"))
-            .body(&include_bytes!("../assets/404.jpg")[..]);
+            .body(include_bytes!("../assets/404.jpg").as_ref());
     }
-    HttpResponse::Ok().content_type(ContentType::html()).body(
-        r#"<video src="/rickroll.mp4" autoplay loop style="width: 100%; height: 100%"></video>
-        <script>
-            const stop = (e) => {e.preventDefault();e.stopPropagation();}
-            document.documentElement.requestFullscreen();
-            document.addEventListener("keydown", e => {
-                const isRefresh = e.key === "F5" || ((e.ctrlKey || e.metaKey) && e.key === "r");
-                if (isRefresh) stop(e);
-            });
-            document.addEventListener("contextmenu", e => stop(e));
-            window.addEventListener("beforeunload", e => stop(e));
-        </script>"#,
-    )
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("../assets/rickroll.html"))
 }
 
 #[actix_web::main]
@@ -43,8 +33,12 @@ async fn main() -> std::io::Result<()> {
         .parse_filters(&dotenvy::var("RUST_LOG").unwrap_or("info".to_string()))
         .init();
 
-    let port = dotenvy::var("PORT").unwrap_or("8080".to_string());
-    info!("Server is running at http://localhost:{}", port);
+    let port: u16 = dotenvy::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .expect("Invalid PORT value");
+
+    info!("Server is running at http://localhost:{port}");
 
     HttpServer::new(|| {
         App::new()
@@ -56,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .service(rickroll)
             .service(Files::new("/", "./assets").prefer_utf8(true))
     })
-    .bind(("0.0.0.0", port.parse::<u16>().unwrap()))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
